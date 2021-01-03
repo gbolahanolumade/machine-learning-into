eaa989934bbd2adb1560cc9ad2a0bc8586dce911
gbolahanolumade <olumadegbolahan@gmail.com>
2021-01-03 23:27:12 +0100

    Titanic Update

--- titanic.py
+++ titanic.py
@@ -5,3 +5,16 @@ Created on Sat Jan  2 23:10:45 2021
 @author: GbolahanOlumade
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_df=pd.read_csv("train.csv")
+test_df = pd.read_csv("test.csv")
+
+train_df.info()
+test_df.info()
+
+test_df['Survived']= -55
+
+df = pd.concat((train_df,test_df))
